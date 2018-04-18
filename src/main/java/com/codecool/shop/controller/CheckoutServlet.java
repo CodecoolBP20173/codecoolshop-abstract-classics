9095933ba1d23e166fdc9404818519c932154f84
Orsolya Lévai <solya0082@gmail.com>
2018-04-18 13:03:59 +0200

    Added summarizing part

--- src/main/java/com/codecool/shop/controller/CheckoutServlet.java
+++ src/main/java/com/codecool/shop/controller/CheckoutServlet.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.Cart.CartItems;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,14 +12,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/checkout"})
 public class CheckoutServlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        int subTotal = 0;
+        for (Map.Entry<Product,Integer> p: CartItems.cartItems.entrySet()) {
+            Product key = p.getKey();
+            Integer value = p.getValue();
+
+            subTotal += (key.getDefaultPrice()*value);
+        }
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("shoppingItems", CartItems.cartItems);
+        context.setVariable("subTotal", subTotal);
 
         engine.process("product/checkout.html", context, resp.getWriter());
     }
