8c6cd22cb74d1b94b8285fa39e25eb20a0f7525b
Orsolya Lévai <solya0082@gmail.com>
2018-04-19 16:47:11 +0200

    Added paths to redirections

--- src/main/java/com/codecool/shop/controller/CheckoutServlet.java
+++ src/main/java/com/codecool/shop/controller/CheckoutServlet.java
@@ -21,23 +21,47 @@ public class CheckoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         OrderDao orderDataStore = OrderDaoMem.getInstance();
-        Order order = orderDataStore.find(1);
 
-        int subTotal = 0;
-        for (Map.Entry<Product, Integer> p : order.getLineItems().entrySet()) {
-            Product key = p.getKey();
-            Integer value = p.getValue();
+        if (orderDataStore.noOrderPlaced()) {
+            resp.sendRedirect("/cart");
 
-            subTotal += (key.getDefaultPrice() * value);
+        } else {
+            Order order = orderDataStore.find(1);
+
+            int subTotal = 0;
+            for (Map.Entry<Product, Integer> p : order.getLineItems().entrySet()) {
+                Product key = p.getKey();
+                Integer value = p.getValue();
+
+                subTotal += (key.getDefaultPrice() * value);
+            }
+
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            WebContext context = new WebContext(req, resp, req.getServletContext());
+
+            context.setVariable("shoppingItems", order.getLineItems());
+            context.setVariable("subTotal", subTotal);
+
+
+            engine.process("product/checkout.html", context, resp.getWriter());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OrderDaoMem orderDaoMem = OrderDaoMem.getInstance();
+
+        if (!orderDaoMem.noOrderPlaced()) {
+            Order order = orderDaoMem.find(1);
+            order.setCustomerName(req.getParameter("name"));
+            order.setCustomerEmail(req.getParameter("email"));
+            order.setCustomerPhone(req.getParameter("phone"));
+            order.setCustomerBillingAddress(req.getParameter("billingAddress"));
+            order.setCustomerShippingAddress(req.getParameter("shippingAddress"));
+            order.setCustomerPaymentMethod(req.getParameter("paymentMethod"));
         }
 
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-
-        context.setVariable("shoppingItems", order.getLineItems());
-        context.setVariable("subTotal", subTotal);
-
-
-        engine.process("product/checkout.html", context, resp.getWriter());
+        String currentURI = "/payment";
+        resp.sendRedirect(currentURI);
     }
 }
--- src/main/java/com/codecool/shop/controller/PaymentServlet.java
+++ src/main/java/com/codecool/shop/controller/PaymentServlet.java
@@ -21,39 +21,37 @@ public class PaymentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         OrderDao orderDataStore = OrderDaoMem.getInstance();
-        Order order = orderDataStore.find(1);
 
-        int subTotal = 0;
-        for (Map.Entry<Product, Integer> p : order.getLineItems().entrySet()) {
-            Product key = p.getKey();
-            Integer value = p.getValue();
+        if (orderDataStore.noOrderPlaced()) {
+            resp.sendRedirect("/");
 
-            subTotal += (key.getDefaultPrice() * value);
+        } else {
+            Order order = orderDataStore.find(1);
+
+            int subTotal = 0;
+            for (Map.Entry<Product, Integer> p : order.getLineItems().entrySet()) {
+                Product key = p.getKey();
+                Integer value = p.getValue();
+
+                subTotal += (key.getDefaultPrice() * value);
+            }
+
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            WebContext context = new WebContext(req, resp, req.getServletContext());
+
+            context.setVariable("shoppingItems", order.getLineItems());
+            context.setVariable("subTotal", subTotal);
+            context.setVariable("paymentMethod", order.getCustomerPaymentMethod());
+
+            engine.process("product/payment.html", context, resp.getWriter());
         }
-
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-
-        context.setVariable("shoppingItems", order.getLineItems());
-        context.setVariable("subTotal", subTotal);
-        context.setVariable("paymentMethod", order.getCustomerPaymentMethod());
-
-        engine.process("product/payment.html", context, resp.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         OrderDaoMem orderDaoMem = OrderDaoMem.getInstance();
-        Order order = orderDaoMem.find(1);
-        order.setCustomerName(req.getParameter("name"));
-        order.setCustomerEmail(req.getParameter("email"));
-        order.setCustomerPhone(req.getParameter("phone"));
-        order.setCustomerBillingAddress(req.getParameter("billingAddress"));
-        order.setCustomerShippingAddress(req.getParameter("shippingAddress"));
-        order.setCustomerPaymentMethod(req.getParameter("paymentMethod"));
-        String currentURI = "/payment";
-        resp.sendRedirect(currentURI);
+        orderDaoMem.remove(1);
 
+        resp.sendRedirect("/");
     }
 }
