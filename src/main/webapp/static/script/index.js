9c87c52641074c4baaae39283500f1a8c615f521
Adam Grob <ad4mgr0b@gmail.com>
2018-04-22 22:10:28 +0200

    The item is now moving to the cart when the add button is clicked.

--- src/main/webapp/static/script/index.js
+++ src/main/webapp/static/script/index.js
@@ -2,6 +2,7 @@ $(window).scrollTop(document.cookie);
 $( document ).ready(function() {
     var popupActivator = document.getElementById("popoverData").dataset.name;
     var myPopover = document.getElementById("myPopover");
+    var cards =  document.getElementsByClassName('group list-group-image');
 
     myPopover.addEventListener('mouseenter', function () {
         console.log('not entering');
@@ -24,16 +25,21 @@ $( document ).ready(function() {
         console.log('hide')
     });
 
-    console.log(popupActivator);
     if (popupActivator === 'active') {
         $("#myPopover").popover({
             placement: 'left'
         });
 
-        $("#myPopover").popover('show');
+        moveCardToCart(cards);
+
+        setTimeout(function () {
+            $("#myPopover").popover('show');
+        }, 1500);
+
+
         setTimeout(function () {
             $(".popover").fadeOut('slow');
-        }, 2000);
+        }, 5000);
         document.getElementById("popoverData").dataset.name = 'passive';
     }
 
@@ -48,6 +54,58 @@ $( document ).ready(function() {
     
 
 });
+
+
+function moveCardToCart(elements) {
+    var cart = $('#myPopover');
+
+    var elementToMove = $(getElementToMove(elements));
+    if (elementToMove) {
+        var imgclone = elementToMove.clone()
+            .offset({
+                top: elementToMove.offset().top,
+                left: elementToMove.offset().left
+            })
+            .css({
+                'opacity': '1',
+                'position': 'absolute',
+                'width': elementToMove.width(),
+                'height': elementToMove.height(),
+                'z-index': '100'
+            })
+            .appendTo($('body'))
+            .animate({
+                'top': cart.offset().top + 10,
+                'left': cart.offset().left + 10,
+                'width': $('#myPopover').width(),
+                'height': $('#myPopover').height()
+            }, 1000, 'easeInOutExpo');
+
+        setTimeout(function () {
+            cart.effect("shake", {
+                times: 2
+            }, 100);
+        }, 1500);
+
+        imgclone.animate({
+            'width': 0,
+            'height': 0
+        }, function () {
+            $(this).detach()
+        });
+    }
+}
+
+
+function getElementToMove(elements) {
+    console.log(document.getElementById('popupContentId').dataset.id);
+    var elementId =  parseInt(document.getElementById('popupContentId').dataset.id);
+    for (var i = 0; i < elements.length; i++) {
+        if (elementId - 1  === i) {
+            return elements[i];
+        }
+    }
+}
 
 
 
