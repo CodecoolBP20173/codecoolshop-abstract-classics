7f5f3d8b88263565e30aa9b3cba81d1a158fdfac
Orsolya Lévai <solya0082@gmail.com>
2018-05-15 12:24:57 +0200

    Added docstring to the Utils class

--- src/main/java/com/codecool/shop/utils/Utils.java
+++ src/main/java/com/codecool/shop/utils/Utils.java
@@ -1,64 +1,35 @@
 package com.codecool.shop.utils;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
 import org.mindrot.jbcrypt.BCrypt;
 
 
-/*String passwordToHash = "password";
-        byte[] salt = getSalt();
-
-        String securePassword = getSecurePassword(passwordToHash, salt);
-        System.out.println(securePassword); //Prints 83ee5baeea20b6c21635e4ea67847f66
-
-        String regeneratedPassowrdToVerify = getSecurePassword(passwordToHash, salt);
-        System.out.println(regeneratedPassowrdToVerify); //Prints 83ee5baeea20b6c21635e4ea67847f66*/
-
-
+/**
+ * This class is for methods which can be used but they don't belong to any of the software logics.
+ * For working put the following dependency into the pom:
+ *         <dependency>
+ *             <groupId>org.mindrot</groupId>
+ *             <artifactId>jbcrypt</artifactId>
+ *             <version>0.4</version>
+ *         </dependency>
+ */
 public class Utils {
+
+    /**
+     * It makes text to hashed text with salt.
+     * @param plainText (String): text for hashing with salt
+     * @return hashed text (String)
+     */
     public String hashPassword(String plainText) {
         return BCrypt.hashpw(plainText, BCrypt.gensalt(12));
     }
 
+    /**
+     * It checks whether two text are the same. One of them is a hashed text, the other is a plain text.
+     * @param plainTextPassword (String): text for hashing with salt
+     * @param hashedPassword (String): hashed text
+     * @return true or false
+     */
     public Boolean checkPasswords(String plainTextPassword, String hashedPassword) {
         return BCrypt.checkpw(plainTextPassword, hashedPassword);
     }
-
-    /*public static String getSecurePassword(String passwordToHash, byte[] salt) {
-        String generatedPassword = null;
-        try {
-
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(salt);
-
-            byte[] bytes = md.digest(passwordToHash.getBytes());
-
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-
-            generatedPassword = sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return generatedPassword;
-    }
-
-    public Boolean verify_password(String plainTextPassword, String hashedPassword) {
-        plainTextPassword
-        hashed_bytes_password = hashed_password.encode('utf-8')
-        return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
-    }
-
-    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
-    }*/
 }
 
