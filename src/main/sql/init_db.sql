5561f60d6e1755dcdbae2389cd898c8816311af5
Peter Kondacs <kondacspeter@gmail.com>
2018-05-16 12:36:06 +0200

    Fixed order id sequence in database creation.

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -78,7 +78,7 @@ CREATE SEQUENCE orders_id_seq
   NO MAXVALUE;
 
 CREATE TABLE orders (
-    id INTEGER NOT NULL DEFAULT nextval('product_category_id_seq'::regclass),
+    id INTEGER NOT NULL DEFAULT nextval('orders_id_seq'::regclass),
     name VARCHAR(255),
     description VARCHAR(255),
     email VARCHAR(255),
